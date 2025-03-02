56e10ae77e47683d80c638fa9e6c5ec4069d258b
hayoung78 <wendy4332@naver.com>
2025-03-02 19:12:52 +0900

    ➕ css 색상 추가 및 아이콘 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "search1.kakaocdn.net",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
